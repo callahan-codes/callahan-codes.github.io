665d4dc552711a685778e4ae0c8aa9d5e6e2dfc9
Bryce Callahan <brycecallahan75@gmail.com>
2023-12-14 00:59:10 -0600

    First files

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('css').addEventListener('click', function() {
+    location.href = '#'
+}, false);
+
+document.getElementById('js').addEventListener('click', function() {
+    location.href = '#'
+}, false);
+
+document.getElementById('three').addEventListener('click', function() {
+    location.href = '#'
+}, false);
+
+document.getElementById('pen').addEventListener('click', function() {
+    window.open('https://codepen.io/Bryce-Callahan', '_blank');
+}, false);
